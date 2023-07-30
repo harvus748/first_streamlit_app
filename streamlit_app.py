79ca4103fcc494a8d4d1d4980439b1ccb3334fec
harvus748 <140657600+harvus748@users.noreply.github.com>
2023-07-30 15:29:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,7 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
+
 #Now organised
 streamlit.header("Fruityvice Fruit Advice!")
 try:
